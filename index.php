91504ed3a317e68f2c279e20d369d8f337b96634
nutv99 <nutv99@gmail.com>
2019-01-01 17:27:31 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  
 <?php
 
-return;
+
 $access_token = 'N0IzKf3n/tuu23eKxvUEkAY6Afzj8nu+lQYp+FyOAZXSVofsrCArcwRBOJKEbssASNnN5S35vUE5yiQ3dPcvlRqu9G0IVPHVxUHUHW63dUUUdxfcWpbZUj7iu8ImPFKK8LnAdy5wGDxvMhUD1A1fugdB04t89/1O/w1cDnyilFU=';
 
 $sValue= getInputMessage() ;
